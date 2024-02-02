e80817bb1c821297cc18c73b32080b39e4ed1a03
sureshpnuri <105036172+sureshpnuri@users.noreply.github.com>
2024-02-01 20:01:00 -0800

    v2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const secondDisplay = document.querySelector(".seconds .number");
 
 const countdownContainer = document.querySelector(".countdown-container");
 
-const endDate = new Date("July 31 2020 17:00:00");
+const endDate = new Date("July 31 2025 17:00:00");
 let saleEnded = false;
 
 const updateTimer = () => {
